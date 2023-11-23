ab450f41384383c4ae10be46c538747ad7cf1806
Nicolas Castro <nicolas.y0909@gmail.com>
2023-11-23 19:15:19 -0300

    feat(src/@core/infra/http/protocols/http-status-code.enum): criação da enumeração dos códigos dos status http.

---
+++ src/@core/infra/http/protocols/http-status-code.enum.ts
@@ -0,0 +1,10 @@
+export enum HttpStatusCodeEnum {
+    ok = 200,
+    created = 201,
+    noContent = 204,
+    badRequest = 400,
+    unauthorized = 401,
+    forbidden = 403,
+    notFound = 404,
+    serverError = 500
+  }
