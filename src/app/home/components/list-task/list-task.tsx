955bf89fd234edf49d7dcc5aaab10f9fc2fe9ca3
Nicolas Castro <nicolas.y0909@gmail.com>
2023-11-29 21:07:24 -0300

    feat(src/app/home/components/list-task/list-task): inserindo o identificador da tarefa nas props.

--- src/app/home/components/list-task/list-task.tsx
+++ src/app/home/components/list-task/list-task.tsx
@@ -1,17 +1,18 @@
-import { TaskProps } from "~/@core/contracts/services/auth/auth";
+import { TaskAPIResponse } from "~/@core/contracts/services/auth/auth";
 import { TaskTag } from "../task";
 
 interface ListTaskProps {
-    tasks: TaskProps[];
+    tasks: TaskAPIResponse[];
 }
 
 export default function ListTask({ tasks }: ListTaskProps) {
     return (
         <article id='tasks' className='mt-10 flex rounded-lg flex-wrap gap-4'>
-            {(tasks.length || 0) > 0 ? tasks.map((task, index) => {
+            {(tasks.length || 0) > 0 ? tasks.map(({id, props: task}, index) => {
               return (
                 <TaskTag 
                   key={index}
+                  id={id}
                   title={task.title}
                   description={task.description}
                   finishAt={task.finishAt}
